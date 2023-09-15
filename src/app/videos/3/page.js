c48244d15ea4341193809c2963e6d13c917b100e
twotw2023 <145062557+twotw2023@users.noreply.github.com>
2023-09-15 11:25:38 +0200

    Update page.js

--- src/app/videos/3/page.js
+++ src/app/videos/3/page.js
@@ -3,9 +3,9 @@ const video3 = () => {
   return (
     <div className='embed-container'>
       <iframe
-        width='560'
-        height='315'
-        src='https://www.youtube.com/watch?v=8hhA01gcCuk'
+        width='315'
+        height='560'
+        src='https://www.youtube.com/embed/8hhA01gcCuk?si=LlGU8PUEwUvqu0SL'
         title='YouTube video player'
         frameborder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
