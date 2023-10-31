e1a24297e1cb48b97d497aef8dd42f5b7352bc3d
twotw2023 <twotw2023@gmail.com>
2023-10-31 22:55:09 +0100

    1st try lazy loading

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 'use client';
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Hero from './components/Hero';
+// import Hero from './components/Hero';
 import './page.scss';
 import Image from 'next/image';
+
+// Client Components:
+const Hero = dynamic(() => import('./components/Hero'));
 
 export default function Home() {
   return (
