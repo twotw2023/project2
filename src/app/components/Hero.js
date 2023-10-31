2b3b7288e1eedeb631ea474a896a607423fa1b17
twotw2023 <twotw2023@gmail.com>
2023-10-31 23:22:06 +0100

    2st try lazy fixing video

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -5,10 +5,16 @@ import './Hero.css';
 const Hero = () => {
   const handleVideoReady = (event) => {
     event.target.play();
+    console.log(event.target);
+  };
+
+  const handlePlay = (event) => {
+    console.log(loaded);
   };
   return (
     <div className='video__container'>
       <video
+        onLoad={handlePlay}
         autoPlay
         muted
         playsInline
@@ -17,9 +23,21 @@ const Hero = () => {
         height={900}
         onCanPlay={handleVideoReady}
       >
-        <source src='/video_hero.mp4' type='video/mp4' />
+        <source
+          src='https://res.cloudinary.com/dhakhocyv/video/upload/v1696146457/project/video_hero_aamxev.mp4'
+          type='video/mp4'
+        />
         Your browser does not support the video tag.
       </video>
+      {/* <iframe
+        src='/video_hero.mp4'
+        loading='lazy'
+        autoPlay
+        className='background-video'
+        width={900}
+        height={900}
+        playsInline
+      ></iframe> */}
     </div>
   );
 };
