de3caf98218603ad65cfeff17cc0b80311190f8c
twotw2023 <twotw2023@gmail.com>
2023-10-31 12:06:27 +0100

    fixing video not playing

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -3,6 +3,9 @@ import React from 'react';
 import './Hero.css';
 
 const Hero = () => {
+  const handleVideoReady = (event) => {
+    event.target.play();
+  };
   return (
     <div className='video__container'>
       <video
@@ -12,6 +15,7 @@ const Hero = () => {
         className='background-video'
         width={900}
         height={900}
+        onCanPlay={handleVideoReady}
       >
         <source src='/video_hero.mp4' type='video/mp4' />
         Your browser does not support the video tag.
