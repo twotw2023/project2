227cf86f473d6d8cde259a29e2e299c0065a42aa
twotw2023 <twotw2023@gmail.com>
2023-12-27 21:48:33 +0100

    fixed featured artists

--- src/app/lib/featured.js
+++ src/app/lib/featured.js
@@ -1,43 +1,36 @@
 export const cardDetails = {
   0: {
-    image: '',
-    title: 'Text 1',
+    image: '/featured/samaneh_roghani.png',
     artist: 'Samneh Roghani',
   },
 
   1: {
-    image: '',
-    title: 'Text 2',
-    artist: 'Ahoora',
+    image: '/featured/Acalypca.png',
+    artist: 'Acalypca',
   },
 
   2: {
-    image: '',
-    title: 'Text 3',
-    artist: 'Ice Flower',
+    image: '/featured/hossein-Fatemi.png',
+    artist: 'Hossein Fatemi',
   },
 
   3: {
-    image: '',
-    title: 'Text 4',
-    artist: 'Nasser Teimoor Pour',
+    image: '/featured/Ice-flower.png',
+    artist: 'Ice Flower',
   },
 
   4: {
-    image: '',
-    title: 'Us',
-    artist: 'Samneh Roghani',
+    image: '/featured/naser-teimoorpour.png',
+    artist: 'Nasser Teymourpour',
   },
 
   5: {
-    image: '',
-    title: 'Execution',
-    artist: 'Nasser Teimoor Pour',
+    image: '/featured/sahel-samavati.png',
+    artist: 'Sahel Samavati',
   },
 
   6: {
-    image: '',
-    title: 'Text 7',
-    artist: 'Samneh Roghani',
+    image: '/featured/shabnam-faraei.png',
+    artist: 'Shabnam Faraee',
   },
 };
